b14d2122fa38b35a191ad7395ded78670aed99ed
villonger <s24665@pjwstk.edu.pl>
2024-03-23 22:48:33 +0100

    new branch commit 2

--- Class1.cs
+++ Class1.cs
@@ -5,6 +5,6 @@ public class Class1
 	public Class1()
 	{
 		console.writeline("hello repo three");
-        console.writeline("hello new branch");
+        console.writeline("hello new branch, hello again");
     }
 }
